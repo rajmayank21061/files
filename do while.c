4b4bde30a6ac6d767e4c3283ddd1e4729d861873
Rj <mayankraj1944@email.com>
2023-06-05 23:27:29 +0530

    c files and another error change

---
+++ do while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+
+int i=23;
+do{
+    printf("\n%d",i);
+i=i+1;
+}
+while(i<9);
+return 0;
+}
---
+++ pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+   int i,j,n;
+   printf("enter the ub value of n");
+   scanf("%d",& n);
+   for(i=0;i<=n;i++)
+   {
+       for(j=0;j<=i;j++)
+       {
+          //if( i+j>=n-1)
+          //{
+              printf("*");
+         // }
+         // else
+          //{
+             // printf(" ");
+          }
+              printf("\n");
+          }
+       
+    return 0;
+}
